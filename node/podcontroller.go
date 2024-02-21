da0b2927b3bad6f451f3625ca1306fd033edb001
jipperinbham <jonphill9@gmail.com>
2024-02-21 14:51:49 -0600

    set span attribute for why pod is not equal

--- node/podcontroller.go
+++ node/podcontroller.go
@@ -361,7 +361,7 @@ func (pc *PodController) Run(ctx context.Context, podSyncWorkers int) (retErr er
 				}
 				kPod.Unlock()
 
-				if podShouldEnqueue(oldPod, newPod) {
+				if podShouldEnqueue(ctx, oldPod, newPod) {
 					pc.syncPodsFromKubernetes.Enqueue(ctx, key)
 				}
 			}
